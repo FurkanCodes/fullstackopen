da1fc3df3db80cc6f53d4d5e4e22d1f1bc185a83
furkanturkoglu <turkogluvelifurkan@gmail.com>
2023-03-26 20:22:30 +0300

    4.16*: bloglist expansion, step4 DONE

--- part4/bloglist/controller/users.js
+++ part4/bloglist/controller/users.js
@@ -6,11 +6,24 @@ usersRouter.post("/", async (req, res) => {
   const { username, password, name } = req.body;
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  if (!username || !password) {
+    return res.status(400).json({
+      error: "password or username missing",
+    });
+  }
+
+  if (password.length < 3) {
+    return res.status(400).json({
+      error: "password is shorter than 3",
+    });
+  }
   const user = new User({
     username,
     name,
     passwordHash,
   });
+
   const savedUser = await user.save();
   res.status(201).json(savedUser);
 });
