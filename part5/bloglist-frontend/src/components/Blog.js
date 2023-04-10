1b2378ae0ff0604e42929a1f771101e28a8d0601
furkanturkoglu <turkogluvelifurkan@gmail.com>
2023-04-10 23:01:03 +0300

    5.7 Blog list frontend, step7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} {blog.likes} {blog.url}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [visible, setVisible] = useState(false);
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <ol>
+        <li>
+          <p> Title </p>
+        </li>{" "}
+        {blog.title}{" "}
+        <div style={showWhenVisible}>
+          <li>
+            <p> Author </p>
+          </li>{" "}
+          {blog.author}{" "}
+          <li>
+            <p> Likes </p>
+          </li>{" "}
+          {blog.likes}
+        </div>
+      </ol>
+      <button onClick={toggleVisibility}>view details</button>
+    </div>
+  );
+};
 
 export default Blog;
