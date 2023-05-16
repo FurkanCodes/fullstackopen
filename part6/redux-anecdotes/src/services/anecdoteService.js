f6f8ad34aabacd525bc3b9a822ea18139de29da0
furkanturkoglu <turkogluvelifurkan@gmail.com>
2023-05-17 00:40:40 +0300

    6.17 Anecdotes and the backend, step4 DONE

--- part6/redux-anecdotes/src/services/anecdoteService.js
+++ part6/redux-anecdotes/src/services/anecdoteService.js
@@ -17,4 +17,12 @@ const createNew = async (content) => {
   return response.data;
 };
 
-export default { getAll, createNew };
+const update = async (objectToUpdate) => {
+  const response = await axios.put(
+    `${baseUrl}/${objectToUpdate.id}`,
+    objectToUpdate
+  );
+  return response.data;
+};
+
+export default { getAll, createNew, update };
