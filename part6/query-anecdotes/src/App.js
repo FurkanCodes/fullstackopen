38644945a4ab35055ddd988f25dd9cf2b95f1749
furkanturkoglu <turkogluvelifurkan@gmail.com>
2023-05-19 16:41:24 +0300

    Exercise 6.20 DONE

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,41 @@
+import { useQuery } from "react-query";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { getAnecdotes } from "./components/requests";
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+  const result = useQuery("anecdotes", getAnecdotes, { retry: false });
+
+  console.log(result);
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  } else if (result.isError) {
+    return <span>Anecdote service is offline</span>;
+  }
+  const anecdotes = result.data;
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
