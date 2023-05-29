2cbd71ef5cb6a751dd5abce8183205151a8f01b2
furkan turkoglu <turkogluvelifurkan@gmail.com>
2023-05-29 22:33:27 +0300

    9.3 Command line done

--- part9/calculateBmi.ts
+++ part9/calculateBmi.ts
@@ -11,5 +11,7 @@ const calculateBmi = (height: number, weight: number) => {
     }
 }
 
+const height: number = Number(process.argv[2])
+const weight: number = Number(process.argv[3])
 
 console.log(calculateBmi(180, 74))
